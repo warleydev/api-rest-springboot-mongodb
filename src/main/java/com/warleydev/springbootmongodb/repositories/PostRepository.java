2311d08c416318525c0046b5a8ceafa16b80395e
Warley Dev <warleycm7@gmail.com>
2023-05-13 17:44:25 -0300

    Fazendo uma consulta simples com query methods

--- src/main/java/com/warleydev/springbootmongodb/repositories/PostRepository.java
+++ src/main/java/com/warleydev/springbootmongodb/repositories/PostRepository.java
@@ -4,6 +4,10 @@ import com.warleydev.springbootmongodb.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    List<Post> findByTitleContainingIgnoreCase(String text);
 }
--- src/main/java/com/warleydev/springbootmongodb/services/PostService.java
+++ src/main/java/com/warleydev/springbootmongodb/services/PostService.java
@@ -6,6 +6,7 @@ import com.warleydev.springbootmongodb.services.exception.ObjectNotFoundExceptio
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,5 +18,9 @@ public class PostService {
     public Post findById(String id){
         Optional<Post> post = repository.findById(id);
         return post.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
+    }
+
+    public List<Post> findByTitle(String text){
+        return repository.findByTitleContainingIgnoreCase(text);
     }
 }
