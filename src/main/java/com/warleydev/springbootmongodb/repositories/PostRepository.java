05a692296fc401b2237dea80f499149683de8eda
Warley Dev <warleycm7@gmail.com>
2023-05-13 17:55:55 -0300

    Realizando a mesma consulta porém de outra forma, utilizando @Query

--- src/main/java/com/warleydev/springbootmongodb/repositories/PostRepository.java
+++ src/main/java/com/warleydev/springbootmongodb/repositories/PostRepository.java
@@ -2,12 +2,16 @@ package com.warleydev.springbootmongodb.repositories;
 
 import com.warleydev.springbootmongodb.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    @Query("{ 'title': {$regex: ?0, $options: 'i' } }")
+    List<Post> findByTitle(String text);
 
     List<Post> findByTitleContainingIgnoreCase(String text);
 }
