2f736fafd5c51055650601d06e8c60343b3f65bb
Warley Dev <warleycm7@gmail.com>
2023-05-15 16:42:54 -0300

    Criando uma consulta utilizando vários critérios. Finalização do projeto

--- src/main/java/com/warleydev/springbootmongodb/resources/PostResource.java
+++ src/main/java/com/warleydev/springbootmongodb/resources/PostResource.java
@@ -1,5 +1,6 @@
 package com.warleydev.springbootmongodb.resources;
 
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
 import com.warleydev.springbootmongodb.domain.Post;
 import com.warleydev.springbootmongodb.resources.util.URL;
 import com.warleydev.springbootmongodb.services.PostService;
@@ -7,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -26,6 +28,19 @@ public class PostResource {
     public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text){
         text = URL.decodeParam(text);
         List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/fullsearch")
+    public ResponseEntity<List<Post>> fullSearch(
+            @RequestParam(value = "text", defaultValue = "")String text,
+            @RequestParam(value = "minDate", defaultValue = "")String minDate,
+            @RequestParam(value = "maxDate", defaultValue = "")String maxDate){
+        text = URL.decodeParam(text);
+        Date min = URL.convertDate(minDate, new Date(0L));
+        Date max = URL.convertDate(maxDate, new Date());
+
+        List<Post> list = service.fullSearch(text, min, max);
         return ResponseEntity.ok().body(list);
     }
 }
--- src/main/java/com/warleydev/springbootmongodb/resources/util/URL.java
+++ src/main/java/com/warleydev/springbootmongodb/resources/util/URL.java
@@ -3,6 +3,10 @@ package com.warleydev.springbootmongodb.resources.util;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class URL {
     public static String decodeParam(String text){
@@ -11,6 +15,18 @@ public class URL {
         }
         catch (UnsupportedEncodingException e){
             return "";
+        }
+    }
+
+    public static Date convertDate(String textDate, Date defaultDate){
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
+        try{
+            return sdf.parse(textDate);
+        }
+        catch (ParseException e){
+            return defaultDate;
         }
     }
 }
